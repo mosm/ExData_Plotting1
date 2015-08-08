6ee040678730de318dd7bdb1608172d5255db442
mosm <mosm@me.com>
2015-08-08 16:29:04 +0200

    3rd and 4th plot added
    Scripts and results for 3rd and 4th plot added for Course Project 1

--- plot3.R
+++ plot3.R
@@ -20,12 +20,27 @@ colnames(DateTime) <- c("DateTime")
 subpower <- cbind(DateTime, subpower)
 
 #Setting up png device
-png(filename="plot2.png", width=480, height=480)
+png(filename="plot3.png", width=480, height=480)
 
-#setting up plot with DateTime on x-axis and Global Active Power on y-axis
-#setting x-label to empty and setting appropriate y-label
-plot(subpower$DateTime, subpower$Global_active_power, type="l", 
-     xlab="", ylab="Global Active Power (kilowatts)")
+#setting up plot with the appropriate scales
+#Setting type to n to suppress plots
+plot(subpower$DateTime, subpower$Sub_metering_1, type="n", xlab="", 
+     ylab="Energy sub metering")
+
+#adding lines for sub_metering1 in black
+lines(subpower$DateTime, subpower$Sub_metering_1, type="l", col="black")
+
+#adding lines for sub_metering2 in red
+lines(subpower$DateTime, subpower$Sub_metering_2, type="l", col="red")
+
+#adding lines for sub_metering3 in blue
+lines(subpower$DateTime, subpower$Sub_metering_3, type="l", col="blue")
+
+#adding legend topright, indicating lines through lty
+#with appropriate colors and legend texts
+legend("topright", lty=c(1,1), col=c("black", "red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
 
 #Closing device, following this step the PNG file is available in the 
 #working directory
